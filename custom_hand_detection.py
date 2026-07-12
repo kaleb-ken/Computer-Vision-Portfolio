661ac2e34ff4f3646a745dda1bb97a8538833a7e
csps319 <chinmay2006singh@gmail.com>
2026-07-12 23:57:01 +1000

    some weird shit goin on with dlib and face rec

---
+++ custom_hand_detection.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+from torchvision.datasets as ImageFolder
+import timm
+
+import matplotlib.pyplot as plt #for data viz
+import pandas as pd 
+import numpy as np
